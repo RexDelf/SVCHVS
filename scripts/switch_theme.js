eab181f70d914f8581151e71e9928d6387ffa0f4
Dominic <jadoro03@gmail.com>
2022-11-29 21:24:40 +0300

    feat: add images

--- scripts/switch_theme.js
+++ scripts/switch_theme.js
@@ -1,26 +1,55 @@
 const themeBtns = document.querySelectorAll('.theme-switch *');
 const themedElements = document.querySelectorAll("[data-dark-mode]");
+const themedImages = document.querySelectorAll("[data-img]");
+const sabImage = document.querySelector('.single-img img');
 
 function changeTheme(theme){
     themeBtns.forEach(el => el.classList.remove('switch-hidden'));
     document.querySelector(`.${theme}`).classList.add('switch-hidden');
     storage_theme = theme;
     
-    theme === 'light' ? enableLightMode() : enableDarkMode();
+    theme === 'light' ? enableLightMode(theme) : enableDarkMode(theme);
 }
 
-function enableLightMode(){
+function enableLightMode(theme){
     themedElements.forEach(el => {
         let key = el.getAttribute("data-dark-mode");
-        el.classList.remove(theme_attributes[key])
+        el.classList.remove(theme_attributes[key]);
+    });
+
+    themedElements.forEach(el => {
+        let key = el.getAttribute("data-img");
+        el.src = theme_images[theme][key];
     });
 }
 
-function enableDarkMode(){
+function enableDarkMode(theme){
     themedElements.forEach(el => {
         let key = el.getAttribute("data-dark-mode");
-        el.classList.add(theme_attributes[key])
+        el.classList.add(theme_attributes[key]);
     });
+
+    themedElements.forEach(el => {
+        let key = el.getAttribute("data-img");
+        el.src = theme_images[theme][key];
+    });
+}
+
+const theme_images = {
+    "light": {
+        "sab": "img/pictures/sab-desktop.png",
+        "lrg": "img/pictures/lrg-desktop.png",
+        "tsowf": "img/pictures/tsowf-desktop.png",
+        "cts": "img/pictures/cts-desktop.png",
+        "lotos": "img/pictures/lotos-desktop.png"
+    },
+    "dark": {
+        "sab": "img/pictures/darkmode/sab-dm.jpg",
+        "lrg": "img/pictures/darkmode/lrg-dm.jpg",
+        "tsowf": "img/pictures/darkmode/tsowf-dm.jpg",
+        "cts": "img/pictures/darkmode/cts-dm.jpg",
+        "lotos": "img/pictures/darkmode/lotos-dm.jpg"
+    }
 }
 
 const theme_attributes = {
